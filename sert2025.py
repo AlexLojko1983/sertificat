555e1790bbc8b13f5a5500c51bef4b08a22e97be
pc <a_l_g@tut.by>
2025-10-20 16:04:00 +0300

    2025

--- sert2025.py
+++ sert2025.py
@@ -10,12 +10,12 @@ import io
 # ==== Настройки ====sudo apt-get install python3-venv
 
 template_pdf = "template.pdf"
-output_pdf = "barcodes_40_2.pdf"
+output_pdf = "barcodes_40_4.pdf"
 x_pos_mm = 220
 y_pos_mm = 62
 
-start_number = 40100000000137
-end_number = 40100000000186
+start_number = 40100000000237
+end_number = 40100000000286
 
 # ==== Параметры writer ====
 writer_options = {
