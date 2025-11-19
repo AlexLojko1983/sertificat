09c0c8dcec4722288af6061764627405bb851fd0
pc <a_l_g@tut.by>
2025-11-19 11:28:43 +0300

    2025_new_voron

--- sert_new.py
+++ sert_new.py
@@ -9,13 +9,13 @@ import io
 from copy import deepcopy
 
 # ==== Настройки ====#
-template_pdf = "сертификат_200.pdf"
-output_name_pattern = "barcodes_VK_200_{}.pdf"  # {} будет номер пачки
+template_pdf = "сертификат_100.pdf"
+output_name_pattern = "barcodes_Voron_100_{}.pdf"  # {} будет номер пачки
 x_pos_mm = 220
 y_pos_mm = 62
 
-start_number = 20010000000720
-end_number = 20010000001438
+start_number = 10010000000101
+end_number = 10010000000397
 
 batch_size = 50  # <--- создаём отдельный файл каждые 50 шт.
 
