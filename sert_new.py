464a3c5a5749f0e72b8f4fab79a834f1e7cc719e
pc <a_l_g@tut.by>
2026-02-04 11:47:53 +0300

    2026_kojdispanser_30

--- sert_new.py
+++ sert_new.py
@@ -9,13 +9,13 @@ import io
 from copy import deepcopy
 
 # ==== Настройки ====#
-template_pdf = "сертификат_cash_50.pdf"
-output_name_pattern = "barcodes_50_{}.pdf"  # {} будет номер пачки
+template_pdf = "сертификат_30_koj.pdf"
+output_name_pattern = "barcodes_30_koj_{}.pdf"  # {} будет номер пачки
 x_pos_mm = 220
 y_pos_mm = 62
 
-start_number = 50100000008711
-end_number = 50100000008712
+start_number = 30100000002202
+end_number = 30100000002395
 
 batch_size = 50  # <--- создаём отдельный файл каждые 50 шт.
 
