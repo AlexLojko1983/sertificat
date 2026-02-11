1e3124afcde4d62e1753e8c554f889eacf580c2c
pc <a_l_g@tut.by>
2026-02-11 14:01:40 +0300

    2026_gigiena_20

--- sert_new.py
+++ sert_new.py
@@ -9,13 +9,13 @@ import io
 from copy import deepcopy
 
 # ==== Настройки ====#
-template_pdf = "сертификат_50_2026.pdf"
-output_name_pattern = "barcodes_50_belajarus_{}.pdf"  # {} будет номер пачки
+template_pdf = "сертификат_20_190226.pdf"
+output_name_pattern = "barcodes_20_sport_{}.pdf"  # {} будет номер пачки
 x_pos_mm = 220
 y_pos_mm = 62
 
-start_number = 50100000008787
-end_number = 50100000008816
+start_number = 10200000001608
+end_number = 10200000001637
 
 batch_size = 50  # <--- создаём отдельный файл каждые 50 шт.
 
