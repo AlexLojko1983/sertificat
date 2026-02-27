21122a5d7bd11859efa2b0b56c1e7120c61a78b9
pc <a_l_g@tut.by>
2026-02-27 16:52:33 +0300

    dadd75ete license

--- sert_new.py
+++ sert_new.py
@@ -11,13 +11,13 @@ import io
 from copy import deepcopy
 
 # ==== Настройки ====#
-template_pdf = "сертификат_cash_30.pdf"
-output_name_pattern = "barcodes_30_cash_{}.pdf"  # {} будет номер пачки
+template_pdf = "сертификат_75_cash.pdf"
+output_name_pattern = "barcodes_75_cash_{}.pdf"  # {} будет номер пачки
 x_pos_mm = 220
 y_pos_mm = 62
 
-start_number = 30100000002906
-end_number = 30100000002927
+start_number = 75100000000001
+end_number = 75100000000017
 
 batch_size = 50  # <--- создаём отдельный файл каждые 50 шт.
 
